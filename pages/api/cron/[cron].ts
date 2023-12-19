f59c00c6c44f993b6ca706d788a6e47fdc06486d
Peter Mbanugo <p.mbanugo@yahoo.com>
2023-12-19 19:51:14 +0100

    Update [cron].ts

--- pages/api/cron/[cron].ts
+++ pages/api/cron/[cron].ts
@@ -8,11 +8,7 @@ export const config = {
 export default async function handler(req: NextRequest) {
   const cron = req.nextUrl.pathname.split('/')[3]
   console.log(cron)
-  if (!cron) return new Response('No cron provided', { status: 400 })
-  const response = await update(cron)
-  return new NextResponse(JSON.stringify(response), {
-    status: 200,
-  })
+  return new Response('Hello', { status: 200 })
 }
 
 async function update(interval: string) {
